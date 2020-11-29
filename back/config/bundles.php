f5b7402dacf0acacd78aca1b7758219d91ed5697
Serhii <anaira682190@gmail.com>
2020-11-29 09:11:04 +0200

    Added symfony debug toolbar.

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -2,5 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
