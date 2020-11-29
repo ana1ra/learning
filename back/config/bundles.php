534aabbea28642e517d8d41f9865cbb6d01d1497
Serhii <anaira682190@gmail.com>
2020-11-29 09:24:14 +0200

    Added GraphqlLite Lib

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
+    TheCodingMachine\Graphqlite\Bundle\GraphqliteBundle::class => ['all' => true],
 ];
