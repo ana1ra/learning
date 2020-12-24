34637ac4b0977ce616a75820b9e35f8513a7fc44
Serhii <anaira682190@gmail.com>
2020-12-24 14:51:27 +0200

    Временно удалил symfony.

--- back/config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
-    TheCodingMachine\Graphqlite\Bundle\GraphqliteBundle::class => ['all' => true],
-];
